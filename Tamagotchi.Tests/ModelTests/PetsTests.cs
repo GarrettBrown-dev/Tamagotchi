7c2c3047f06ffc202d31e447d11a2e1e46601f7a
Ben Stoller <stollersystemsound@gmail.com>
2020-09-30 13:51:11 -0700

    start project
    Co-authored by: Garrett Brown <gman9mm@live.com>
    co-Authored-by: Natalie Murphy <nataliemurphy500@gmail.com>
    Co-authored-by: Mike Manchee <mikemanchee@gmail.com>

---
+++ Tamagotchi.Tests/ModelTests/PetsTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tamagotchi.Models;
+using System.Collections.Generic;
+using System;
+
+namespace Tamagotchi.Tests
+{
+  [TestClass]
+  public class PetTests : IDisposable
+  {
+    [TestMethod]
+    public void Pet_InstantiateAnInstanceOfPet_newPlace()
+    {
+      //Arrange
+
+      //Act
+
+      // Assert
+
+    }
+  }
+}
